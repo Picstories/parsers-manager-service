0c6eee5587a30271e1cc0e6b3bad91b2b16a2446
arman.shamenov <arman.shamenov@corp.mail.ru>
2020-04-21 19:26:59 +0500

    init

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM openjdk:12-jdk-alpine
+COPY parsers-manager-service-1.0-SNAPSHOT.jar app.jar
+ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ src/main/java/org/picstories/manager/parsers/ParsersManagerApplication.java
@@ -0,0 +1,14 @@
+package org.picstories.manager.parsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author arman.shamenov
+ */
+@SpringBootApplication
+public class ParsersManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ParsersManagerApplication.class, args);
+    }
+}
