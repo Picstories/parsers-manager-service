c23745e58dd4c1eb0750944a125aa98cd0c11f81
arman.shamenov <arman.shamenov@corp.mail.ru>
2020-04-26 15:38:18 +0500

    fetch new props

--- src/main/java/org/picstories/manager/parsers/ParsersManagerApplication.java
+++ src/main/java/org/picstories/manager/parsers/ParsersManagerApplication.java
@@ -3,8 +3,6 @@ package org.picstories.manager.parsers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -12,8 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
-@EnableReactiveMongoRepositories(basePackages = "org.picstories.library.repository")
-@EnableReactiveElasticsearchRepositories
 @EntityScan({
         "org.picstories.library.model.entity"
 })
