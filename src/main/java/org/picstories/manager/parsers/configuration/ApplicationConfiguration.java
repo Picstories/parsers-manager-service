ba15698ece2048922d7daf93ddfd2c919a5e5b19
arman.shamenov <arman.shamenov@corp.mail.ru>
2020-04-27 19:37:05 +0500

    fix transaction

--- src/main/java/org/picstories/manager/parsers/configuration/ApplicationConfiguration.java
+++ src/main/java/org/picstories/manager/parsers/configuration/ApplicationConfiguration.java
@@ -2,20 +2,10 @@ package org.picstories.manager.parsers.configuration;
 
 import org.picstories.library.elasticsearch.ReactiveElasticsearchConfiguration;
 import org.picstories.library.mapper.PicstoriesMapper;
-import org.picstories.library.model.kafka.parsers.UpdateTask;
 import org.picstories.library.mongodb.ReactiveMongoDbConfiguration;
-import org.picstories.manager.parsers.service.AppRunner;
-import org.picstories.manager.parsers.service.UpdateComicsService;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
-import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
-import org.springframework.transaction.ReactiveTransactionManager;
-import org.springframework.transaction.reactive.TransactionalOperator;
-import reactor.kafka.receiver.KafkaReceiver;
 
 /**
  * @author arman.shamenov
@@ -30,23 +20,5 @@ public class ApplicationConfiguration {
     @Bean
     public PicstoriesMapper picstoriesMapper() {
         return new PicstoriesMapper();
-    }
-
-    @Bean
-    public ReactiveTransactionManager rtm(@Qualifier("reactiveMongoDbFactory") ReactiveMongoDatabaseFactory dbf) {
-        return new ReactiveMongoTransactionManager(dbf);
-    }
-
-    @Bean
-    public TransactionalOperator tm(ReactiveTransactionManager rtm) {
-        return TransactionalOperator.create(rtm);
-    }
-
-    @Bean
-    public ApplicationRunner applicationRunner(
-            KafkaReceiver<String, UpdateTask> kafkaReceiver,
-            UpdateComicsService updateService
-    ) {
-        return new AppRunner(kafkaReceiver, updateService);
     }
 }
--- src/main/java/org/picstories/manager/parsers/service/UpdateComicsService.java
+++ src/main/java/org/picstories/manager/parsers/service/UpdateComicsService.java
@@ -1,6 +1,7 @@
 package org.picstories.manager.parsers.service;
 
 import com.google.common.collect.Iterables;
+import com.mongodb.MongoClientException;
 import org.picstories.library.model.entity.comics.Comics;
 import org.picstories.library.model.entity.comics.ComicsMD;
 import org.picstories.library.model.entity.page.PageMD;
@@ -9,8 +10,12 @@ import org.picstories.library.repository.mongo.ComicsMongoRepository;
 import org.picstories.library.repository.mongo.PageMongoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.reactive.TransactionalOperator;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
+import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverRecord;
 
 import java.time.LocalDateTime;
@@ -21,25 +26,24 @@ import java.util.stream.Collectors;
  * @author arman.shamenov
  */
 @Service
-public class UpdateComicsService {
+public class UpdateComicsService implements ApplicationRunner {
     private static final Logger logger = LoggerFactory.getLogger(UpdateComicsService.class);
 
     private final PageMongoRepository pageRepos;
     private final ComicsMongoRepository comicsRepos;
 
-    private final TransactionalOperator transactionalOperator;
+    private final KafkaReceiver<String, UpdateTask> receiver;
 
     public UpdateComicsService(PageMongoRepository pageRepos,
                                ComicsMongoRepository comicsRepos,
-                               TransactionalOperator transactionalOperator) {
+                               KafkaReceiver<String, UpdateTask> receiver) {
         this.pageRepos = pageRepos;
         this.comicsRepos = comicsRepos;
-        this.transactionalOperator = transactionalOperator;
+        this.receiver = receiver;
     }
 
-
-    public void updateComics(ReceiverRecord<String, UpdateTask> record) {
-
+    @Transactional(rollbackFor = MongoClientException.class)
+    public Mono<ComicsMD> updateComics(ReceiverRecord<String, UpdateTask> record) {
         UpdateTask task = record.value();
 
         List<PageMD> pageList = task.getPages().stream().map(PageMD::new).collect(Collectors.toList());
@@ -48,14 +52,19 @@ public class UpdateComicsService {
         comic.setLastUpdate(LocalDateTime.now());
         comic.setPageCount(lastPage.getNumber());
 
-        transactionalOperator.execute(status -> pageRepos.saveAll(pageList)
+        return pageRepos.saveAll(pageList)
                 .doOnNext(page -> logger.info("page save = {} ", page))
                 .doOnError(err -> logger.error("Error saving page ", err))
                 .then(comicsRepos.save(new ComicsMD(comic)))
                 .doOnNext(com -> logger.info("comic save = {}", com))
-                .doOnError(com -> {
-                    logger.error("error saving comic ", com);
-                    status.setRollbackOnly();
-                })).subscribe();
+                .doOnError(com -> logger.error("error saving comic ", com));
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        receiver.receive()
+                .flatMap(this::updateComics)
+                .doOnError(error -> logger.error("Error while execute in transaction update comics : ", error))
+                .subscribe();
     }
 }
